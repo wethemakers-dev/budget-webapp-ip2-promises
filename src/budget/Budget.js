68209965c643447fb1c475f98b0d52b5ab2ab6ab
Ali AbuDiab <aliabudiab112@gmail.com>
2020-02-24 16:15:19 +0200

    Modify the App.js file by adding the Route and put the Navbar class in the budget page

--- src/budget/Budget.js
+++ src/budget/Budget.js
@@ -2,15 +2,24 @@ import React from "react";
 import InputBudget from "./InputBudget";
 import InputExpenses from "./InputExpenses";
 import DisplayBudget from "../display/DisplayBudget";
+import Navbar from "../navbar";
+import "../App.css";
 const Budget = () => {
   return (
-    <div className="row">
-      <div className="col-lg-4">
-        <InputBudget />
-        <InputExpenses />
-      </div>
-      <div className="col-lg-8">
-        <DisplayBudget />
+    <div>
+      <div className="App">
+        <Navbar />
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-lg-4">
+              <InputBudget />
+              <InputExpenses />
+            </div>
+            <div className="col-lg-8">
+              <DisplayBudget />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
